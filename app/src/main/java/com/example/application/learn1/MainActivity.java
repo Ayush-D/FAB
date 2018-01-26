599e78f6a8ddbbdc71011a0295835def50be3bc7
Ayush <https://ayush1111@bitbucket.org/fcb_ved/user_app.git>
2018-01-26 13:55:44 +0530

    first commit

---
+++ app/src/main/java/com/example/application/learn1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.application.learn1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    //write here your code
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
